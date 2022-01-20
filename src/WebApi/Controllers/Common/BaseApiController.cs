06006b964ef660325832d24e28bbe8d45f386d89
caner.demirci <caner.demirci@trendyol.com>
2022-01-20 19:43:12 +0300

    Base controller created.

---
+++ src/WebApi/Controllers/Common/BaseApiController.cs
@@ -0,0 +1,13 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers.Common;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class BaseApiController : ControllerBase
+{
+    private IMediator _mediator;
+
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+}
