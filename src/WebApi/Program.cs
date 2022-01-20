4134b28a3a60dcd0f24bd86edbd929e64bf5e099
caner.demirci <caner.demirci@trendyol.com>
2022-01-20 19:44:33 +0300

    Added more api configuration.

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,11 +1,38 @@
+using Application;
+using FluentValidation.AspNetCore;
+using Infrastructure;
+using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddPersistenceRegistration();
+builder.Services.AddInfrastructureRegistration();
+builder.Services.AddApplicationRegistration();
+builder.Services.AddControllers(options =>
+        options.Filters.Add<ApiExceptionFilterAttribute>())
+    .AddFluentValidation(x => x.AutomaticValidationEnabled = false); //ValidationBehaviour handle fluent validations.
+
+#region Api Versioning
+builder.Services.AddApiVersioning(config =>
+{
+    // Specify the default API Version as 1.0
+    config.DefaultApiVersion = new ApiVersion(1, 0);
+    // If the client hasn't specified the API version in the request, use the default API version number 
+    config.AssumeDefaultVersionWhenUnspecified = true;
+    // Advertise the API versions supported for the particular endpoint
+    config.ReportApiVersions = true;
+});
+#endregion
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
 
 var app = builder.Build();
 
